2c4cfd78ceed6a89adb8b9c6ee48b77cc67a562e
JarroVGIT <jarro@vanginkel-it.nl>
2022-06-17 12:33:52 +0200

    4739 try outs (streamingresponse with gzip)

--- 5034/app.py
+++ 5034/app.py
@@ -1,30 +1,48 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse, Response
+import typing
+from fastapi import BackgroundTasks, FastAPI, File, UploadFile
+from fastapi.responses import FileResponse, StreamingResponse, JSONResponse
+from starlette.background import BackgroundTask
 import uvicorn
 
 app = FastAPI()
 
+@app.get("/with_direct_jsonresponse")
+async def with_direct_jsonresponse():
+    return JSONResponse(content="{'hello':'world']", status_code=408) 
+    # 408 will not be documented! Will only show a 200 as per default response class
 
-class ImageResponse(Response):
-    media_type = "image/*"
+@app.get("with_direct_jsonresponse_and_responses", 
+    responses={406:{"description":"in-line 406 response wut?"}})
+async def with_direct_jsonresponse_and_responses():
+    return JSONResponse(content="{'hello':'world']") 
+    # This would document both 200 (as per default response class) and 406
 
-    # What do I need to add here to specify that 'format = "binary"'
+class CustomJSONResponseWith407(JSONResponse):
+    media_type = "application/json"
+    def __init__(
+        self,
+        content: typing.Any,
+        status_code: int = 407,
+        headers: typing.Optional[dict] = None,
+        media_type: typing.Optional[str] = None,
+        background: typing.Optional[BackgroundTask] = None,
+    ) -> None:
+        super().__init__(content, status_code, headers, media_type, background)
+
+@app.get("/with_response_class_as_streamingresponse", response_class=StreamingResponse) 
+async def with_response_class_as_streamingresponse():
+    return {"is this": "a 40x?"}
+    # documents only a 407, as the response class is set to that.
+
+@app.get("/with_response_class_and_responses", 
+    response_class=CustomJSONResponseWith407, 
+    responses={406:{"description":"in-line 406 response wut?"}}) 
+async def with_response_class():
+    return {"is this": "a 407?"}
+    # documents only a 407 as the response class is set to that,
+    # But also a 406.
 
 
-# here I would like to no longer have to specify 'responses' but only use 'response_class' as a single source of information
-@app.post("what_I_currently_do",
-    response_class=ImageResponse,
-    responses={200: {"content": {
-        "image/*": {
-            "schema":{
-                "type":"string",
-                "format": "binary"
-            }
-        }
-    }}},
-)
-async def send_g(b: UploadFile = File(...)):
-    return "/tmp/test/b.jpg"
-
-
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, )    
 
